510a21592699381a108513d3976785fd19158f80
Lucas Leonardo <xmiiitz@gmail.com>
2025-05-19 00:08:02 -0300

    chore: update props cardinfo

--- src/app/(publics)/page.tsx
+++ src/app/(publics)/page.tsx
@@ -3,8 +3,7 @@ import { CardInfo } from '@/components/CardInfo'
 const data = [
   {
     id: 1,
-    title: 'Related Articles',
-    subtitle: 'Getting Started with TypeScript',
+    title: 'Getting Started with TypeScript',
     description:
       "A beginner's guide to TypeScript and its benefitsA beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.",
     image: '/img.png',
@@ -12,41 +11,45 @@ const data = [
   {
     id: 2,
 
-    subtitle: 'Getting Started with TypeScript',
-    description: "A beginner's guide to TypeScript and its benefits.",
+    title: 'Getting Started with TypeScript',
+    description:
+      "A beginner's guide to TypeScript and its benefitsA beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.",
     image: '/img2.png',
   },
   {
     id: 3,
-    title: 'Related Articles',
-    subtitle: 'Getting Started with TypeScript',
-    description: "A beginner's guide to TypeScript and its benefits.",
+
+    title: 'Getting Started with TypeScript',
+    description:
+      "A beginner's guide to TypeScript and its benefitsA beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.",
     image: '/img2.png',
   },
   {
     id: 4,
-    title: 'Related Articles',
-    subtitle: 'Getting Started with TypeScript',
-    description: "A beginner's guide to TypeScript and its benefits.",
-    image: '/img.png',
+
+    title: 'Getting Started with TypeScript',
+    description:
+      "A beginner's guide to TypeScript and its benefitsA beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.A beginner's guide to TypeScript and its benefits.",
+    image: '/img2.png',
   },
 ]
 
-const Page = () => {
+export default function Page() {
   return (
-    <div className="flex flex-wrap max-w-full">
-      {data.map((card) => (
-        <div key={card.id} className="w-full lg:w-1/4 md:w-1/2 p-3">
-          <CardInfo
-            title={card.title}
-            subtitle={card.subtitle}
-            description={card.description}
-            image={card.image}
-          />
+    <div className="container">
+      <div className="flex flex-col gap-4 w-full py-4">
+        <h1 className="text-lg font-bold">Related Articles</h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-x-9 gap-y-11 grid-col-1 w-full">
+          {data.map((card) => (
+            <CardInfo
+              key={card.id}
+              title={card.title}
+              description={card.description}
+              image={card.image}
+            />
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   )
 }
-
-export default Page
--- src/components/secaodestaque.tsx
+++ src/components/secaodestaque.tsx
@@ -1,18 +1,16 @@
-
 export default function Secaodestaque() {
   return (
     <section className="w-full h-screen bg-gray-100 flex items-center justify-center px-4">
       <div className="max-w-7xl mx-auto text-center">
         <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-gray-900 text-center max-w-3xl mx-auto leading-tight">
-        Beautiful UI Components for Modern Applications
+          Beautiful UI Components for Modern Applications
         </h1>
 
-
         <p className="mt-4 text-gray-600 text-base md:text-lg leading-snug max-w-3xl mx-auto">
-  Build stunning interfaces with our professionally designed, fully customizable UI components. <br />
-  Perfect for React, Vue, and Angular projects.
-</p>
-
+          Build stunning interfaces with our professionally designed, fully
+          customizable UI components. <br />
+          Perfect for React, Vue, and Angular projects.
+        </p>
 
         <div className="mt-6 flex justify-center gap-4">
           <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-medium py-2 px-6 rounded">
@@ -24,5 +22,5 @@ export default function Secaodestaque() {
         </div>
       </div>
     </section>
-  );
+  )
 }
